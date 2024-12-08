ce6bea51e4125053c8a4c6591ea585ede3323f61
ShirinkinTima <timofej.shirinkin@gmail.com>
2024-12-09 02:20:12 +0500

    main file

---
+++ krestikinoliki.cpp
@@ -0,0 +1,18 @@
+﻿
+int main() {
+    Board board;
+    bool isXTurn = true;
+
+    while (true) {
+        board.print();
+
+        int row, col;
+        std::cout << (isXTurn ? "X" : "O") << ", введите координаты (строка, столбец): ";
+        std::cin >> row >> col;
+        row--;
+        col--;
+
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board.getCell(row, col) != CellState::None) {
+            std::cout << "Неверные координаты. Попробуйте еще раз.\n";
+            continue;
+        }
