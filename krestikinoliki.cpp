9842d3b75746d4d870be6e4b702ef6f31bbeab6a
ShirinkinTima <timofej.shirinkin@gmail.com>
2024-12-09 11:52:21 +0500

    UpdateMainKrestikinoliki.cpp

--- krestikinoliki.cpp
+++ krestikinoliki.cpp
@@ -1,18 +1,40 @@
-﻿
 int main() {
-    Board board;
-    bool isXTurn = true;
+    char currentPlayer = X;
+    bool gameOver = false;
 
-    while (true) {
-        board.print();
+    while (!gameOver) {
+        // Вывод игрового поля
+        printBoard();
 
+        // Получение хода игрока
         int row, col;
-        std::cout << (isXTurn ? "X" : "O") << ", введите координаты (строка, столбец): ";
-        std::cin >> row >> col;
-        row--;
-        col--;
+        cout << "Игрок " << currentPlayer << ", введите номер строки и номер столбца (от 1 до 3): ";
+        cin >> row >> col;
 
-        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board.getCell(row, col) != CellState::None) {
-            std::cout << "Неверные координаты. Попробуйте еще раз.\n";
+        // Валидация хода
+        if (row < 1  col < 1  board[row - 1][col - 1] != EMPTY) {
+            cout << "Неверный ход. Попробуйте снова." << endl;
             continue;
         }
+
+        // Выполнение хода
+        board[row - 1][col - 1] = currentPlayer;
+
+        // Проверка на выигрыш
+        if (checkWin(currentPlayer)) {
+            gameOver = true;
+            cout << "Игрок " << currentPlayer << " победил!" << endl;
+        }
+
+        // Ничья
+        else if (isBoardFull()) {
+            gameOver = true;
+            cout << "Ничья!" << endl;
+        }
+
+        // Переключение игрока
+        currentPlayer = (currentPlayer == X) ? O : X;
+    }
+
+    return 0;
+}
